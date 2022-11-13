b52a36ac310774d16b887480fe7992f996cd7e8d
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-14 01:19:59 +0530

    initial project setup

---
+++ src/main/java/com/full_stack_Final_Assessment/fullStackFinalAssessment/FullStackFinalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.full_stack_Final_Assessment.fullStackFinalAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackFinalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackFinalAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/full_stack_Final_Assessment/fullStackFinalAssessment/FullStackFinalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full_stack_Final_Assessment.fullStackFinalAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackFinalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
