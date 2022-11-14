d6fdc972a64fdf0423578216090b4c0c0cbddcca
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-14 16:31:21 +0530

    Added shopManagementService  - dependencies  - application & cloud properties

---
+++ shopManagementService/src/main/java/com/shop/management/service/shopManagementService/ShopManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.management.service.shopManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopManagementServiceApplication.class, args);
+	}
+
+}
